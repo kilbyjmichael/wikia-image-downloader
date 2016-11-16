1b21f70b77a69f9c1c52dbea9241936338ed0216
Michael <kilbyjmichael@gmail.com>
2016-11-15 19:57:53 -0600

    status writing

--- rs_image_get.py
+++ rs_image_get.py
@@ -3,7 +3,7 @@ import json
 import csv
 import rfc6266
 
-data_file = 'rs_data.test'
+data_file = 'rs_data.dat'
 
 base_link = 'http://2007.runescape.wikia.com/api/v1/Articles/AsSimpleJson?id='
 
@@ -15,8 +15,10 @@ for row in csv_reader:
     data = json.loads(page.text)
     try:
         image_url = data['sections'][0]['images'][0]['src']
-        image_file = requests.get(image_url)        
-        with open(rfc6266.parse_requests_response(image_file).filename_unsafe, 'wb') as img_file:
+        image_file = requests.get(image_url)
+        img_filename = rfc6266.parse_requests_response(image_file).filename_unsafe
+        with open(img_filename, 'wb') as img_file:
             img_file.write(image_file.content)
+            print("WROTE: " + img_filename)
     except (IndexError, KeyError) as e:
         pass
